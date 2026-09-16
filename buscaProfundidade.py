bc51990a327475669d182919ff87197aabc8d392
CERTEZZA\gabriel.alvise <gabriel.alvise@certezza.com.br>
2026-09-16 17:47:43 -0300

    Aprimorando busca profundidade

--- buscaProfundidade.py
+++ buscaProfundidade.py
@@ -3,3 +3,23 @@ from labirinto import labirinto, posicaoInicial, posicaoFinal, obterVizinhos
 def buscaProfundidade():
     pilha = [(posicaoInicial, [posicaoInicial])]
     visitados = set()
+
+    while pilha:
+        posicaoAtual, caminho = pilha.pop()
+
+        if posicaoAtual == posicaoFinal:
+            return caminho
+
+        if posicaoAtual in visitados:
+            continue
+
+        visitados.add(posicaoAtual)
+
+        vizinhos = obterVizinhos(posicaoAtual)
+
+        for vizinho in vizinhos:
+            if vizinho not in visitados:
+                novoCaminho = caminho + [vizinho]
+                pilha.append((vizinho, novoCaminho))
+
+    return None
